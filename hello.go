b948642fc0603090b809a8fe51446769de4850bd
albertoparente <albertoparentefh@gmail.com>
2021-12-25 23:09:49 -0300

    Adding intro to function, continuation

--- hello.go
+++ hello.go
@@ -2,43 +2,25 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
 )
 
 func main() {
-	name := "Alberto Parente"
-	age := 29
-	version := 1.0
-	fmt.Println("Hello world, my name is", name)
-	fmt.Println("My age is,", age)
-	fmt.Println("This version is,", version)
 
-	name2 := "Alberto Parente"
-	age2 := 29
-	version2 := 1.0
-	fmt.Println("Hello world, my name is", name2, "my age is", age2)
-	fmt.Println("This version is,", version2)
-	fmt.Println("The type of variable name is:", reflect.TypeOf((name2)))
+	showIntroduction()
+	showMenu()
+	command := readCommand()
 
-	fmt.Println("1- Starting monitoring")
-	fmt.Println("2- Show logs")
-	fmt.Println("0- Exit the program")
-
-	var command int
-	fmt.Scan(&command)
-	fmt.Println("The memory address is:", &command)
-	fmt.Println("The command chosen was:", command)
-	
 	/*
-	if command == 1 {
-		fmt.Println("Monitoring...")
-	} else if command == 2 {
-		fmt.Println("Viewing logs...")
-	} else if command == 0 {
-		fmt.Println("Leaving the program...")
-	} else {
-		fmt.Println("Invalid command...!")
-	}
+		if command == 1 {
+			fmt.Println("Monitoring...")
+		} else if command == 2 {
+			fmt.Println("Viewing logs...")
+		} else if command == 0 {
+			fmt.Println("Leaving the program...")
+		} else {
+			fmt.Println("Invalid command...!")
+		}
 	*/
 
 	switch command {
@@ -48,20 +30,30 @@ func main() {
 		fmt.Println("Viewing logs...")
 	case 0:
 		fmt.Println("Leaving the program...")
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid command...!")
+		os.Exit(-1)
 	}
 }
 
 func showIntroduction() {
-    name := "Alberto Parente"
-    version := 1.1
-    fmt.Println("Hello, sr.", name)
-    fmt.Println("This program is in version", version)
+	name := "Alberto Parente"
+	version := 1.1
+	fmt.Println("Hello, sr.", name)
+	fmt.Println("This program is in version", version)
+}
+
+func showMenu() {
+	fmt.Println("1- Starting monitoring")
+	fmt.Println("2- Show logs")
+	fmt.Println("0- Exit the program")
 }
 
 func readCommand() int {
-    var commandRead int
-    fmt.Scan(&commandRead)
-    fmt.Println("The chosen command was", commandRead)
+	var commandRead int
+	fmt.Scan(&commandRead)
+	fmt.Println("The chosen command was", commandRead)
 
-    return commandRead
+	return commandRead
 }
