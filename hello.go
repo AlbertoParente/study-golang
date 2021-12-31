c43c956a41e4b01ecbab25c6116c9cd1321cc21a
albertoparente <albertoparentefh@gmail.com>
2021-12-30 23:26:47 -0300

    Adding delay and loop

--- hello.go
+++ hello.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const numberOfMonitoring = 3
+const delay = 5
 
 func main() {
 
@@ -48,6 +52,7 @@ func readCommand() int {
 	var commandRead int
 	fmt.Scan(&commandRead)
 	fmt.Println("The chosen command was", commandRead)
+	fmt.Println("")
 
 	return commandRead
 }
@@ -57,22 +62,26 @@ func startingMonitoring() {
 	sites := []string{
 		"https://www.innovaro.com.br",
 		"http://sync.innovaro.com.br/sync/",
-		"http://licenca.innovaro.com.br/allylicenca/",
-		"http://app.allysistemas.com.br/sync/",
-	}
+		"http://licenca.innovaro.com.br/allylicenca/"}
 
-	for i, site := range sites {
-		fmt.Println(i, site)
+	for i := 0; i < numberOfMonitoring; i++ {
+		for i, site := range sites {
+			fmt.Println(i+1, site)
+			siteTests(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
 	}
+	fmt.Println("")
 }
 
 func siteTests(site string) {
 
-    resp, _ := http.Get(site)
+	resp, _ := http.Get(site)
 
-    if resp.StatusCode == 200 {
-        fmt.Println("The Site:", site, "was successfully loaded!")
-    } else {
-        fmt.Println("The Site:", site, "did not load successfully! Status code:", resp.StatusCode)
-    }
+	if resp.StatusCode == 200 {
+		fmt.Println("The Site:", site, "was successfully loaded!")
+	} else {
+		fmt.Println("The Site:", site, "did not load successfully! Status code:", resp.StatusCode)
+	}
 }
