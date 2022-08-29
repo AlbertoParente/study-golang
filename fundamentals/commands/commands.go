76419c4998ac5b5271a52069c635d496b3a54704
albertoparente <albertoparentefh@gmail.com>
2022-08-28 23:56:01 -0300

    Adding concepts

---
+++ fundamentals/commands/commands.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Prompt commands.")
+	/*
+	   go
+	   go help
+	   go help get
+	   go version
+	   godoc -http=:6060
+	   go env
+	   go doc cmd/vet
+	   go vet "file.go"
+	   go build "file.go"
+	   go run "file.go"
+	*/
+}
