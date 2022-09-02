c1d8099cb1febef100c8e2ad93b8cdefc0b58834
albertoparente <albertoparentefh@gmail.com>
2022-09-01 22:50:00 -0300

    Adding types concept, continuation

--- fundamentals/types-conceps/types.go
+++ fundamentals/types-conceps/types.go
@@ -19,10 +19,31 @@ func main() {
 
 	var i2 rune = 'a'
 	fmt.Println("The rune is ", reflect.TypeOf(i2))
-	fmt.println(i2)
+	fmt.Println(i2)
 
 	var x float32 = 49.99
 	fmt.Println("O type of x is:", reflect.TypeOf(x))
 	fmt.Println("The type of literal is:", reflect.TypeOf(49.99))
+
+	bo := true
+	fmt.Println("O type of bo is:", reflect.TypeOf(bo))
+	fmt.Println(!bo)
+
+	s1 := "Hello"
+	fmt.Println(s1 + "!")
+	fmt.Println("The size of string is:", len(s1))
+
+	s2 := `Hello
+    my
+    name
+    is
+    Alberto Parente`
+	fmt.Println(s2 + "!")
+	fmt.Println("The size of string is:", len(s1))
+
+	// Char not exists in linguage Go
+	char := 'a'
+	fmt.Println("The type of char is:", reflect.TypeOf(char))
+	fmt.Println(char) // return value is unicode
 
 }
