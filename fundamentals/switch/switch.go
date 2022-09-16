f01272bed2e20e399cae9ad24fb677170671bc87
albertoparente <albertoparentefh@gmail.com>
2022-09-15 22:19:48 -0300

    Adding switch concept

---
+++ fundamentals/switch/switch.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func noteToConcept(n float64) string {
+	var note = int(n)
+	switch note {
+	case 10:
+		fallthrough
+	case 9:
+		return "A"
+	case 8, 7:
+		return "B"
+	case 6, 5:
+		return "C"
+	case 4, 3:
+		return "D"
+	case 2, 1, 0:
+		return "E"
+	default:
+		return "Invalid note"
+	}
+}
+
+func main() {
+	fmt.Println(noteToConcept(10))
+	fmt.Println(noteToConcept(9.8))
+	fmt.Println(noteToConcept(6.9))
+	fmt.Println(noteToConcept(2.1))
+	fmt.Println(noteToConcept(1))
+	fmt.Println(noteToConcept(-1.1))
+}
