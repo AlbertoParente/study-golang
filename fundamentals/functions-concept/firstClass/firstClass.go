d4709bfe0e404d6a449b98dd93d65f04f62a6bb9
albertoparente <albertoparentefh@gmail.com>
2022-09-27 22:20:19 -0300

    Adding class concept

---
+++ fundamentals/functions-concept/firstClass/firstClass.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var summ = func(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Fprintln(summ(2, 3))
+	sub := func(a, b int) int {
+		return a - b
+	}
+
+	div := func(a, b int) int {
+		return a / b
+	}
+
+	mult := func(a, b int) int {
+		return a * b
+	}
+
+	fmt.Println(sub(2, 3))
+	fmt.Println(div(10, 2))
+	fmt.Println(mult(5, 3))
+}
